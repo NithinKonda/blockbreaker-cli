0359bc59e37a2d15dbce224d14071d9b8ffc1439
NithinKonda <nithinkonda142@gmail.com>
2025-03-21 23:42:57 +0530

    add color setup for BlockBreaker game using curses

--- python/blockbreaker.py
+++ python/blockbreaker.py
@@ -21,3 +21,15 @@ class BlockBreaker:
         self.setup_colors()
         
         self.reset_game()
+
+
+    def setup_colors(self):
+        if curses.has_colors():
+            curses.start_color()
+            curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)   # Ball
+            curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)   # Paddle
+            curses.init_pair(3, curses.COLOR_CYAN, curses.COLOR_BLACK)    # Blocks 1
+            curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)    # Blocks 2
+            curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK) # Blocks 3
+            curses.init_pair(6, curses.COLOR_RED, curses.COLOR_BLACK)     # Game over
+            curses.init_pair(7, curses.COLOR_YELLOW, curses.COLOR_BLACK)  # Score
