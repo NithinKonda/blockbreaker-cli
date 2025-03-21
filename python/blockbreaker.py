60f5fa943ed7ee2d698cf4232f14b6a2255a75e0
NithinKonda <nithinkonda142@gmail.com>
2025-03-21 23:42:19 +0530

    add BlockBreaker class with initial setup for curses screen

--- python/blockbreaker.py
+++ python/blockbreaker.py
@@ -2,3 +2,10 @@ import curses
 import random
 import time
 import math
+
+class BlockBreaker:
+    def __init__(self, screen):
+        self.screen = screen
+        curses.curs_set(0)
+        self.screen.timeout(0)
+        self.height, self.width = self.screen.getmaxyx()
