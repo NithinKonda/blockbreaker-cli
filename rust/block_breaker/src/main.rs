a93f408b7891232f5ae45490ab66e2a71dcd4965
NithinKonda <nithinkonda142@gmail.com>
2025-03-23 00:30:50 +0530

    add crossterm imports for terminal control and event handling

--- rust/block_breaker/src/main.rs
+++ rust/block_breaker/src/main.rs
@@ -1,3 +1,12 @@
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode, KeyEvent},
+    execute,
+    style::{Color, Print, SetBackgroundColor, SetForegroundColor},
+    terminal::{self, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
+    Result,
+};
+
 fn main() {
     println!("Hello, world!");
 }
