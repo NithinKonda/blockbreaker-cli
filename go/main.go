6a4239639d52a91490fe0a725aaf63d55311a0af
NithinKonda <nithinkonda142@gmail.com>
2025-03-25 23:43:07 +0530

    add BlockBreaker struct to manage game state and properties in BlockBreaker

--- go/main.go
+++ go/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gdamore/tcell/v2"
 )
 
@@ -10,4 +12,35 @@ type Block struct {
 	width int
 	color tcell.Color
 	hit   bool
+}
+
+type BlockBreaker struct {
+	screen     tcell.Screen
+	width      int
+	height     int
+	paddleChar rune
+	ballChars  []rune
+	ballFrame  int
+	blockChar  rune
+	paddleSize int
+
+	ballSpeed   float64
+	paddleSpeed float64
+
+	paddleX float64
+	paddleY float64
+
+	ballX  float64
+	ballY  float64
+	ballDX float64
+	ballDY float64
+
+	blocks []Block
+
+	score            int
+	lives            int
+	gameOver         bool
+	gameWon          bool
+	lastUpdate       time.Time
+	animationCounter int
 }
